626c045ea0f411f93dc523e63918f2488a80aa0b
fruzelee <fazlerabbicse@gmail.com>
2020-06-13 06:22:26 +0600

    Google News Features:
    - Consumed newsapi.org API instead of Google News API as it's no longer available.
    
    - Used retrofit for making api calls.
    
    - Stored these responses in SQLite database.
    
    - Used Room persistence library to provide an abstraction layer over SQLite.
    
    - Showed response data in a recycler view inside the app main page.
    
    - Added Swipe Refresh to reload the contents from the screen.
    
    - These data have faced from the Database and not from API.
    
    - When clicked, it browse to the news site to show the news details.
    
    - Browser have implemented inside the app.
    
    - Browser is able to play videos in full screen. [Special case]
    
    - Used the MVVM pattern with Kotlin and Android Architecture Components.

--- app/src/main/java/com/crevado/fr/googlenews/adapter/CommonBindingAdapter.kt
+++ app/src/main/java/com/crevado/fr/googlenews/adapter/CommonBindingAdapter.kt
@@ -4,7 +4,24 @@ import androidx.appcompat.widget.AppCompatImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
-
+/**
+ * Created by FazLe Rabbi on 13,June,2020
+ * fazle.rabbi.cse@gmail.com
+ * <p>
+ * Copyright (c) 2020 FazLe Rabbi
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 @BindingAdapter("imageUrlCache", "placeholder")
 fun loadImageCache(view: AppCompatImageView, imageUrlCache: String?, placeholder: Int) {
 
--- app/src/main/java/com/crevado/fr/googlenews/util/Constants.kt
+++ app/src/main/java/com/crevado/fr/googlenews/util/Constants.kt
@@ -1,7 +1,24 @@
 package com.crevado.fr.googlenews.util
 
 import com.crevado.fr.googlenews.R
-
+/**
+ * Created by FazLe Rabbi on 13,June,2020
+ * fazle.rabbi.cse@gmail.com
+ * <p>
+ * Copyright (c) 2020 FazLe Rabbi
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 class Constants {
     companion object {
         const val PLACEHOLDER_TRANSPARENT_IMAGE = R.drawable.bg_transparent
